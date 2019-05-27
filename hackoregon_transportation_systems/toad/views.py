252a0b7dca7357eea3127754ddac8503668ec37e
Jeff Beyer <jeffrey.beyer@nike.com>
2019-05-27 13:02:54 -0700

    adds views for all stops and passenger stops

--- hackoregon_transportation_systems/toad/views.py
+++ hackoregon_transportation_systems/toad/views.py
@@ -1,4 +1,26 @@
-from django.http import HttpResponse, JsonResponse
-from rest_framework.decorators import api_view, detail_route
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+# from django.http import HttpResponse, JsonResponse
+from rest_framework import viewsets
+
+# from rest_framework.decorators import api_view, detail_route
+# from rest_framework.parsers import JSONParser
+# from rest_framework.renderers import JSONRenderer
+from toad.models import BusAllStops, BusPassengerStops
+from toad.serializers import BusAllStopsSerializer, BusPassengerStopsSerializer
+
+
+class BusAllStopsViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset will provide a list of all times a bus stops (scheduled or not).
+    """
+
+    queryset = BusAllStops.objects.all()
+    serializer_class = BusAllStopsSerializer
+
+
+class BusPassengerStopsViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset will provide a list of scheduled stops along a Trimet line.
+    """
+
+    queryset = BusPassengerStops.objects.all()
+    serializer_class = BusPassengerStopsSerializer
