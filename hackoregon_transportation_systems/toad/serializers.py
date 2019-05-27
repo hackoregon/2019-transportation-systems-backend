e0591499a778f65c9c4e10d3dbf1e3a05d2d8439
Jeff Beyer <jeffrey.beyer@nike.com>
2019-05-27 13:02:41 -0700

    Adds serializers for all stops and passenger stops

--- hackoregon_transportation_systems/toad/serializers.py
+++ hackoregon_transportation_systems/toad/serializers.py
@@ -1,1 +1,19 @@
 from rest_framework import serializers
+from rest_framework_gis import serializers
+from toad.models import BusAllStops, BusPassengerStops
+
+
+class BusAllStopsSerializer(serializers.GeoFeatureModelSerializer):
+    class Meta:
+        model = BusAllStops
+        fields = "__all__"
+        geo_field = "geom_4326"
+        id = "id"
+
+
+class BusPassengerStopsSerializer(serializers.GeoFeatureModelSerializer):
+    class Meta:
+        model = BusPassengerStops
+        fields = "__all__"
+        geo_field = "geom_4326"
+        id = "id"
