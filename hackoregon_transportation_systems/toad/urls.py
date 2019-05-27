0aef001e657611fd42b1d210946bde86f9af4362
Jeff Beyer <jeffrey.beyer@nike.com>
2019-05-27 13:03:19 -0700

    Updates local toad urls.

--- hackoregon_transportation_systems/toad/urls.py
+++ hackoregon_transportation_systems/toad/urls.py
@@ -1,16 +1,9 @@
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
-from rest_framework.schemas import get_schema_view
-from rest_framework_swagger.views import get_swagger_view
+from toad import views
 
 router = DefaultRouter()
+router.register(r"bus-all-stops", views.BusAllStopsViewSet)
+router.register(r"bus-passenger-stops", views.BusPassengerStopsViewSet)
 
-
-#schema view
-schema_view = get_swagger_view(title='Transportation Systems 2019 API')
-
-urlpatterns = [
-    url(r'^schema/', schema_view),
-    url
-    url(r'^toad/', include(router.urls)),
-]
+urlpatterns = [url(r"^/", include(router.urls))]
