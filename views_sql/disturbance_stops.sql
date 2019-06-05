e5925f4838a5fe2109dd1cf62e1fed4d04418fa4
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-06-05 14:26:14 -0700

    disturbance_stops view

--- views_sql/disturbance_stops.sql
+++ views_sql/disturbance_stops.sql
@@ -1,8 +1,20 @@
-SELECT bus_all_stops.opd_date, DATE_PART('dow', bus_all_stops.opd_date) AS day_of_week,
-  bus_all_stops.act_arr_time, bus_all_stops.act_dep_time, 
+DROP MATERIALIZED VIEW IF EXISTS public.disturbance_stops CASCADE;
+CREATE MATERIALIZED VIEW public.disturbance_stops
+AS
+SELECT bus_all_stops.opd_date, date_part('dow', bus_all_stops.opd_date) AS day_of_week,
+  bus_all_stops.act_arr_time, bus_all_stops.act_dep_time,
+  0.25 * trunc(4 * date_part('hour', bus_all_stops.act_arr_time) +
+    date_part('minute', bus_all_stops.act_arr_time) / 15) AS start_quarter_hour,
+  0.25 * trunc(4 * date_part('hour', bus_all_stops.act_dep_time) +
+    date_part('minute', bus_all_stops.act_dep_time) / 15) AS end_quarter_hour,
   bus_all_stops.act_dep_time - bus_all_stops.act_arr_time AS duration,
-  bus_trips.line_id, geom_point_4326
+  bus_trips.line_id, bus_trips.pattern_direction,
+  ST_X(geom_point_4326) AS longitude, ST_Y(geom_point_4326) AS latitude, geom_point_4326
 FROM bus_all_stops
-LEFT JOIN bus_trips ON bus_trips.event_no_trip = bus_all_stops.event_no_trip
+INNER JOIN bus_trips ON bus_trips.event_no_trip = bus_all_stops.event_no_trip
 WHERE stop_type = 3
-LIMIT 100;
+
+WITH DATA;
+
+ALTER TABLE public.disturbance_stops
+    OWNER TO transportation2019;
