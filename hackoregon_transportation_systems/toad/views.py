7a93f5c510a3688edc22f491559b7ce120af88b8
Jeffrey Beyer <Jeffrey.Beyer@nike.com>
2019-06-12 20:44:02 -0700

    adds disturbance stop views

--- hackoregon_transportation_systems/toad/views.py
+++ hackoregon_transportation_systems/toad/views.py
@@ -1,11 +1,10 @@
-# from django.http import HttpResponse, JsonResponse
 from rest_framework import viewsets
-
-# from rest_framework.decorators import api_view, detail_route
-# from rest_framework.parsers import JSONParser
-# from rest_framework.renderers import JSONRenderer
-from toad.models import BusAllStops, BusPassengerStops
-from toad.serializers import BusAllStopsSerializer, BusPassengerStopsSerializer
+from toad.models import BusAllStops, BusPassengerStops, DisturbanceStops
+from toad.serializers import (
+    BusAllStopsSerializer,
+    BusPassengerStopsSerializer,
+    DisturbanceStopsSerializer,
+)
 
 
 class BusAllStopsViewSet(viewsets.ReadOnlyModelViewSet):
@@ -24,3 +23,28 @@ class BusPassengerStopsViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = BusPassengerStops.objects.all()
     serializer_class = BusPassengerStopsSerializer
+
+
+class DisturbanceStopsViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset will provide a list of all disturbance stops of busses.
+    """
+
+    # queryset = DisturbanceStops.objects.all()
+    serializer_class = DisturbanceStopsSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the disturbance stops by line by filtering against a
+        `lines` query parameter in the URL.
+        """
+        queryset = DisturbanceStops.objects.all()
+        lines = self.request.query_params.get("lines", None)
+        if lines is not None:
+            queryset = queryset.filter(line_id__in=[int(l) for l in lines.split(",")])
+
+        direction = self.request.query_params.get("direction", None)
+        if direction is not None:
+            queryset = queryset.filter(pattern_direction="I")
+
+        return queryset
