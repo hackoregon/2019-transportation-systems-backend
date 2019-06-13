0104e7312fd38c77ab144a45bbf051f2d14be871
Jeffrey Beyer <Jeffrey.Beyer@nike.com>
2019-06-12 20:43:44 -0700

    adds disturbance stop urls

--- hackoregon_transportation_systems/toad/urls.py
+++ hackoregon_transportation_systems/toad/urls.py
@@ -5,5 +5,11 @@ from toad import views
 router = DefaultRouter()
 router.register(r"bus-all-stops", views.BusAllStopsViewSet)
 router.register(r"bus-passenger-stops", views.BusPassengerStopsViewSet)
+router.register(
+    r"disturbance-stops", views.DisturbanceStopsViewSet, basename="disturbance-stops"
+)
 
-urlpatterns = [url(r"^", include(router.urls))]
+urlpatterns = [
+    url(r"^", include(router.urls)),
+    url(r"^disturbance-stops/(?P<line>[0-9]+)/$", views.DisturbanceStopsViewSet),
+]
