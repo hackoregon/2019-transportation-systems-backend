06f154ddd501dcc1e94ca4d0ac12f97fc1ce8f74
Jeff Beyer <jeff.beyer@gmail.com>
2019-07-30 21:55:39 -0700

    Comments out busAllStops route

--- hackoregon_transportation_systems/toad/urls.py
+++ hackoregon_transportation_systems/toad/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from toad import views
 
 router = DefaultRouter()
-router.register(r"busAllStops", views.BusAllStopsViewSet, basename="bus-all-stops")
+# router.register(r"busAllStops", views.BusAllStopsViewSet, basename="bus-all-stops")
 router.register(
     r"busPassengerStops", views.BusPassengerStopsViewSet, basename="bus-passenger-stops"
 )
