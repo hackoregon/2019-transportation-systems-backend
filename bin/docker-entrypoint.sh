2bffd99c1ac032e229014cca002fc823df14cbff
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-05 23:04:09 -0700

    dump envars after ssm read

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -17,6 +17,9 @@ echo Debug: "${DEBUG,,}"
 
 if [ ! "${DEBUG,,}" ] && [ ! "${TRAVIS}" ]; then
   source /code/bin/get-ssm-parameters.sh
+  POSTGRES_NAME: $POSTGRES_NAME
+  POSTGRES_HOST: $POSTGRES_HOST
+  POSTGRES_PORT: $POSTGRES_PORT
 fi
 
 if [ "$POSTGRES_NAME" ]; then
